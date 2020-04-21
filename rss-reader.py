fc92a6771636a0ab70c53414beb745eb81ea0351
Gary Beardsley <tiresandplanes@gmail.com>
2020-04-21 09:59:15 -0400

    Delete default variable Delete site_address variable Delete file_number variable Change website to content Change feed to parsed_website Change podcast_number to mp3_index Remove feed_length Create if_empty to check self.site Change get_site_size to byte_size Add check for correct input

--- rss-reader.py
+++ rss-reader.py
@@ -12,36 +12,37 @@ from pathlib import Path, PureWindowsPath
 
 class SITE():
 	def __init__(self):
-		self.site_address = ' '
-		self.website = ' '
-		self.default = 'https://lexfridman.com/category/ai/feed/'#http://feeds.noisia.nl/NoisiaRadio
-		self.site = ' '
-		self.feed = ' '
-		self.site_size = 0
-		self.response = ' '
-		self.file_number = 0
-		self.mp3_feed = ' '
-		self.mp3_name = ' '
-		self.feed_length = 0
-		self.podcast_number = None
+		self.content = ' ' # Raw website information
+		self.site = ' ' # Site Address
+		self.site_size = 0 # Size in Bytes
+		self.parsed_site = ' ' # Parsed website
+
+		self.mp3 = ' ' # mp3 url
+		self.mp3_name = ' ' # name of mp3
+		self.mp3_index = 0 # mp3 index
+
+	def if_empty(self):
+		if self.site == ' ':
+			self.site = input('RSS feed to add/download: ')
 
 	def download_site(self):
 		with urllib.request.urlopen(self.site) as f:
-			self.website = f.read(self.site_size).decode('utf-8')
-			print( self.site_size )
+			self.content = f.read(self.site_size).decode('utf-8')
+			print(self.site_size)
+		self.parsed_site = feedparser.parse(self.site)
 	
-		self.feed = feedparser.parse( self.site )
-	
+	# byte size of website
 	def get_site_size(self):
 		# Gets Byte size of website
-		self.response = requests.get(self.site)
-		self.site_size = len(self.response.content)
+		response = requests.get(self.site)
+		self.site_size = len(response.content)
 
 	def print_info(self):
-		for x in range(0,len(self.feed.entries)):
-			print(("%d: %s") % (x ,self.feed.entries[x].title))
+		for x in range(0,len(self.parsed_site.entries)):
+			print(("%d: %s") % (x ,self.parsed_site.entries[x].title))
+		
 		# Prints out entire website
-		# Print( self.website )
+		# Print( self.content )
 
 def create_directories():
 	if os.path.isdir('feeds') and os.path.isdir('config'):
@@ -75,12 +76,15 @@ def get_file_list():
 		else:
 			print("os not recognized")
 
-def options():
-	menu = ['Add new Feed', 'Load Feeds', 'download']
+def options(menu):
+	length = 0
 	for x, i in enumerate(menu):
+		length = length + 1
 		print('%s. %s' % (x, i))
-	menu_option = input("What would you like to do?")
-	return menu_option
+		
+	menu = input("What would you like to do?")
+	return menu, length
+
 
 def save_file(feed_path, config_path, site):
 	s = str(site.site)
@@ -96,10 +100,10 @@ def load_file(feed_path, config_path):
 	for x, i in enumerate(s):
 		print('%s: %s' % (x,i),end='')
 	get_user_input = int(input("What feed would you like to load?"))
+
 	for x, i in enumerate(s):
 		if get_user_input == x:
 			new.site = str(i)
-	
 	download_feed(feed_path, config_path, new)
 
 def add_new_feeds(feed_path, config_path):
@@ -107,78 +111,94 @@ def add_new_feeds(feed_path, config_path):
 	new.site = input('rss feed to add: ')
 	save_file(feed_path, config_path, new)
 
-def download_feed(feed_path, config_path, new_site):
+def download_feed(feed_path, config_path, site):
 	create_directories()
 	sub_directory = get_file_list()
 	print(sub_directory)
-	if new_site.site == ' ':
-		new_site.site = input('RSS feed to add: ')
-	new_site.get_site_size()
-	new_site.download_site()
-	new_site.print_info()
+	
+	site.if_empty()
+	site.get_site_size()
+	site.download_site()
+	site.print_info()
 	numbers = 0
 	
-	new_site.feed_length = len(new_site.feed.entries)-1
-	new_site.podcast_number = int(input('What file would you like to download?'))
-	new_site.mp3_feed = new_site.feed.entries[new_site.podcast_number].links[1].href
+	site.mp3_index = int(input('What file would you like to download?'))
+	site.mp3_feed = site.parsed_site.entries[site.mp3_index].links[1].href
 
 	# Names mp3 file with title of podcast episode
 	# as well as replace any spaces with underscores
-	new_site.mp3_name = new_site.feed.entries[new_site.podcast_number].title[0:20] + '.mp3'
-	new_site.mp3_name.replace(' ', '_')
+	site.mp3_name = site.parsed_site.entries[site.mp3_index].title[0:20] + '.mp3'
+	site.mp3_name.replace(' ', '_')
 	
 	# download preferred mp3
-	answer = input("Are you sure you want to download " + "'" + new_site.feed.entries[new_site.podcast_number].title + "'")
+	answer = input("Are you sure you want to download " + "'" + site.parsed_site.entries[site.mp3_index].title + "'")
 	answer = answer.upper()
 
 	if answer == 'Y':
-		print(feed_path)
-		print(new_site.site)
-		print(new_site.mp3_feed)
-		newnew = new_site.mp3_feed.find('/')
+		print('feed path:' + feed_path)
+		print('site_url' + site.site)
+		print('mp3 feed:' + site.mp3_feed)
+		print('mp3 name:' + site.mp3_name)
+		newnew = site.mp3_feed.find('/')
 		i = 0
-		for x in new_site.mp3_feed:
+		for x in site.mp3_feed:
 			if x == '\\':
 				print(x)
 
 		print(newnew)
-		wget.download(new_site.mp3_feed, feed_path + new_site.mp3_name)
+		wget.download(site.mp3_feed, feed_path + site.mp3_name)
 
-	save_it = input("Would you like to save this feed to use for later?")
+	save_it = input("\nWould you like to save this feed to use for later?")
 	save_it = save_it.upper()
 	
 	if save_it == 'Y':
-		save_file(feed_path, config_path, new_site)
-	
-def main():
+		save_file(feed_path, config_path, site)
+
+def assign_paths():
 	unix_feed_path = './feeds/'
 	unix_config_path = './config/'
 	windows_feed_path = '.\\feeds\\'
 	windows_config_path = '.\\config\\'
-	feed_path = ' '
-	config_path = ' '
 
 	if(platform.system() == 'Linux' or platform.system() == 'Darwin'):
 		feed_path = unix_feed_path
 		config_path = unix_config_path
+		return feed_path, config_path
 	elif platform.system() == 'Windows':
 		feed_path = windows_feed_path
 		config_path = windows_config_path
+		return feed_path, config_path
 	else:
 		print("os not recognized")
 
-	menu_option = options()
+def menu_options():
+	while True:
+		try:
+			menu, length = options(['Add new Feed', 'Load Feeds', 'download'])
+			menu = int(menu)
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		if menu < 0 or menu > length:
+			print("Sorry, your response must within choice range given.")
+			continue
+		else:
+			return menu
 
-	if menu_option == '0':
+def main():
+	feed_path, config_path = assign_paths()
+
+	menu = menu_options()
+	print(menu)
+	
+	if menu == 0:
 		add_new_feeds(feed_path, config_path)
-	elif menu_option == '1':
+	elif menu == 1:
 		load_file(feed_path, config_path)
-	elif menu_option == '2':
-		new_site = SITE()
-		download_feed(feed_path, config_path, new_site)
-	else: 
-		print("none")
-
+	elif menu == 2:
+		site = SITE()
+		download_feed(feed_path, config_path, site)
+	
+	
 if __name__ == '__main__':
 	main()
-	
